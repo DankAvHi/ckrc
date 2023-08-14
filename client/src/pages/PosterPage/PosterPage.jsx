612fea2586e28c0d36d7c51a69a7f940bbee1aa3
DankAvHi <thexcat86@gmail.com>
2023-08-14 11:21:15 +0300

    main

--- client/src/pages/PosterPage/PosterPage.jsx
+++ client/src/pages/PosterPage/PosterPage.jsx
@@ -1,4 +1,4 @@
-import { PageWrapper, PosterPlaceholder1Image, SectionHeading } from "../../shared";
+import { PageWrapper, PosterPlaceholder1Image, PosterPlaceholder2Image, SectionHeading } from "../../shared";
 import styles from "./PosterPage.module.css";
 import Calendar from "react-calendar";
 import "./Calendar.css";
@@ -20,10 +20,34 @@ export const PosterPage = () => {
                             <p
                                 className={styles.eventDescription}
                             >{`Знаменитый рок-музыкант, оказавший невероятное влияние на успешное становление и развитие таких монстров рока, как Deep Purple, Rising Force, Rainbow и других, автор и вокалист Джо Линн Тёрнер, выступит перед российскими фанатами и зрителями с новой концертной программой.`}</p>
+                            <p className={styles.eventData}>
+                                <span
+                                    className={styles.eventDataField}
+                                >{`Дата и время мероприятия: 19 октября 2023 в 19:00`}</span>
+                                <span className={styles.eventDataField}>{`Категория: Концерты`}</span>
+                            </p>
+                            <Link className={styles.eventLink}>{`Подробнее...`}</Link>
+                        </div>
+                    </div>
+                    <div className={styles.event}>
+                        <img className={styles.eventImage} src={PosterPlaceholder2Image} alt="" />
+                        <div className={styles.eventInfo}>
+                            <h2 className={styles.eventHeading}>{`СПЕКТАКЛЬ «СТАРШИЙ СЫН»`}</h2>
+                            <p className={styles.eventTitleData}>
+                                <span className={styles.eventTitleDataField}>{`Стоимость: 3 000 — 6 000 руб.`}</span>
+                                <span
+                                    className={styles.eventTitleDataField}
+                                >{`Продолжительность: 2 часа 40 минут с одним антрактом.`}</span>
+                            </p>
                             <p
-                                className={styles.eventDataField}
-                            >{`Дата и время мероприятия: 19 октября 2023 в 19:00`}</p>
-                            <p className={styles.eventDataField}>{`Категория: Концерты`}</p>
+                                className={styles.eventDescription}
+                            >{`Время, наверное, сейчас такое… Суетное, торопливое, прагматичное, слишком материальное. Хочется найти свой островок, где всегда тепло и уютно, где будут только родные и близкие люди, где тебя примут таким, какой ты есть. Создатели спектакля «Старший сын» попытались воплотить такой мир, куда не доносятся отголоски суеты большого...`}</p>
+                            <p className={styles.eventData}>
+                                <span
+                                    className={styles.eventDataField}
+                                >{`Дата и время мероприятия: 13 октября 2023 в 19:00`}</span>
+                                <span className={styles.eventDataField}>{`Категория: Театр`}</span>
+                            </p>
                             <Link className={styles.eventLink}>{`Подробнее...`}</Link>
                         </div>
                     </div>
--- client/src/widgets/Header/Header.jsx
+++ client/src/widgets/Header/Header.jsx
@@ -25,6 +25,14 @@ export const Header = () => {
                         </p>
                     </li>
                 </ul>
+                <div className={styles.widget}>
+                    <p className={styles.widgetTitle}>{`Сложности с получением «Пушкинской карты»?`}</p>
+                    <a
+                        className={styles.widgetButton}
+                        href="https://pos.gosuslugi.ru/form/?opaId=355440&utm_source=vk&utm_medium=03&utm_campaign=1052301317881"
+                        target="_blank"
+                    >{`Написать`}</a>
+                </div>
                 <ul className={styles.links}>
                     <li className={styles.linkContainer}>
                         <a href="#" className={styles.link}>
